a41a2ed968a52c289187e10c9b2e84f558d34b3d
Milan <mujdar.milan@gmail.com>
2025-02-17 17:25:09 +0200

    Item manager and stats manager, now working on UI

---
+++ Nothing Can Go Wrong/scenes/managers/item_manager.gd
@@ -0,0 +1,35 @@
+extends Node
+
+enum ITEM_TYPE {RAW_FOOD, COOKED_FOOD, WATER, FLASHLIGHT, FLAREGUN, BATTERY}
+
+@onready var current_items : Dictionary = {
+	ITEM_TYPE.RAW_FOOD : 0,
+	ITEM_TYPE.COOKED_FOOD : 0,
+	ITEM_TYPE.WATER : 0,
+	ITEM_TYPE.FLASHLIGHT : 1,
+	ITEM_TYPE.FLAREGUN : 0,
+	ITEM_TYPE.BATTERY : 0
+}
+
+
+@onready var item_to_icon : Dictionary = {
+	ITEM_TYPE.RAW_FOOD : preload("res://assets/icons/food_icon.png"),
+	ITEM_TYPE.COOKED_FOOD : preload("res://assets/icons/cooked_food_icon.png"),
+	ITEM_TYPE.WATER : preload("res://assets/icons/bottle_of_water_icon.png"),
+	ITEM_TYPE.FLASHLIGHT : preload("res://assets/icons/flashlight_icon.png"),
+	ITEM_TYPE.FLAREGUN : preload("res://assets/icons/flaregun_icon.png"),
+	ITEM_TYPE.BATTERY : preload("res://assets/icons/battery_icon.png")
+}
+
+
+func add_items(type: ITEM_TYPE, quantity: int = 1):
+	current_items[type] += quantity
+
+
+func remove_items(type: ITEM_TYPE, quantity: int = 1):
+	if current_items[type] < quantity:
+		push_error("CANNOT REMOVE MORE ITEMS THAN YOU OWN!")
+	
+	current_items[type] -= quantity
+
+
---
+++ Nothing Can Go Wrong/scenes/managers/stats_manager.gd
@@ -0,0 +1,51 @@
+extends Node
+
+enum STAT_TYPE {HUNGER, THIRST, HEALTH, SANITY, ENERGY}
+
+var current_stats : Dictionary = {
+	STAT_TYPE.HUNGER : 0,
+	STAT_TYPE.THIRST : 0,
+	STAT_TYPE.HEALTH : 0,
+	STAT_TYPE.SANITY : 0,
+	STAT_TYPE.ENERGY : 0,
+}
+
+@onready var maximum_stats : Dictionary = {
+	STAT_TYPE.HUNGER : 6,
+	STAT_TYPE.THIRST : 6,
+	STAT_TYPE.HEALTH : 10,
+	STAT_TYPE.SANITY : 6,
+	STAT_TYPE.ENERGY : 10,
+}
+
+func _ready():
+	set_maximum_stats()
+
+
+func increase_maximum_stat(type: STAT_TYPE, amount: int):
+	maximum_stats[type] += amount
+
+
+func decrease_maximum_stat(type: STAT_TYPE, amount: int):
+	if maximum_stats[type] < amount:
+		push_error("CANNOT DECREASE MAXIMUM STAT WITH MORE THAN YOU HAVE")
+	
+	maximum_stats[type] -= amount
+
+
+func increase_stat(type: STAT_TYPE, amount: int):
+	current_stats[type] += amount
+
+
+func decrease_stat(type: STAT_TYPE, amount: int):
+	if current_stats[type] < amount:
+		push_error("CANNOT DECREASE CURRENT STAT WITH MORE THAN YOU HAVE")
+	
+	current_stats[type] -= amount
+
+
+func set_maximum_stats():
+	for type in STAT_TYPE:
+		current_stats[STAT_TYPE[type]] = maximum_stats[STAT_TYPE[type]]
+
+
